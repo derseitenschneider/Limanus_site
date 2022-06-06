812f626103d90917fd54b89d638873f6cdc455d6
Brian Boy <brian.boy@gmx.ch>
2022-06-06 15:18:40 +0200

    Import template

---
+++ dev/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+
+  <script defer src="js/index.js"></script></head>
+  <body>
+    <?php
+require(dirname(__FILE__) . '/includes/header.php')
+    ?>
+    <h1>hallo duse</h1>
+
+    <h2><a href="about.php">About</a></h2>
+
+
+  </body>
+</html>
---
+++ webpack.dev.js
@@ -0,0 +1,70 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const BrowserSyncPlugin = require("browser-sync-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    index: "./src/app/index.js",
+    about: "./src/app/about.js",
+  },
+  output: {
+    filename: "js/[name].js",
+    path: path.resolve(__dirname, "dev"),
+    clean: true,
+  },
+
+  watch: true,
+
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+    },
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "assets/[name].[hash][ext]",
+        },
+      },
+      {
+        test: /\.php$/i,
+        loader: "html-loader",
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Homepage",
+      template: "src/index.php",
+      filename: "index.php",
+      chunks: ["index"],
+    }),
+    new HtmlWebpackPlugin({
+      title: "About",
+      template: "src/pages/about.php",
+      filename: "about.php",
+      chunks: ["about"],
+    }),
+    new HtmlWebpackPlugin({
+      title: "Header",
+      template: "src/includes/header.php",
+      filename: "includes/header.php",
+      chunks: [],
+    }),
+    new BrowserSyncPlugin({
+      host: "localhost",
+      port: 8000,
+      proxy: "http://localhost:8888/webpack-php-setup/dev/",
+    }),
+  ],
+};
