60851748eb9eb75ab07e3175935745528cac6569
Brian Boy <brian.boy@gmx.ch>
2022-06-07 15:12:59 +0200

    lang change in index

--- src/index.php
+++ src/index.php
@@ -1,11 +1,10 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="de">
   <head>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Document</title>
-
   </head>
   <body>
     <?php
@@ -14,7 +13,5 @@ require(dirname(__FILE__) . '/includes/header.php')
     <h1>hallo dusididie</h1>
 
     <h2><a href="about.php">About</a></h2>
-
-
   </body>
 </html>
